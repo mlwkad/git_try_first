947ff3d14f152f312f8612a88e7651a065cc8904
zzz <3502219532@qq.com>
2024-10-08 20:29:24 +0800

    额外

--- cl/src/App.vue
+++ cl/src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="草拟吗的" />
+      <HelloWorld msg="草拟吗的666" />
     </div>
   </header>
 
